2ec5414f89c4f4ada55c056e47394aa61a7001e1
cakradana <rcakradana@gmail.com>
2023-02-14 19:44:53 +0700

    extend colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,21 @@ module.exports = {
     './nuxt.config.{js,ts}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: {
+          50: '#f6edff',
+          100: '#eaddff',
+          200: '#d0bcff',
+          300: '#b69df8',
+          400: '#9a82db',
+          500: '#7f67be',
+          600: '#6750a4',
+          700: '#4f378b',
+          800: '#381e72'
+        }
+      }
+    }
   },
   plugins: []
 }
